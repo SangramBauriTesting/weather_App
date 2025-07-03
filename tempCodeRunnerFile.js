b7b289fcea6e86326b39a0375269d9a5c4d29e84
SangramBauriTesting <sangramdeveloper1@gmail.com>
2025-07-03 23:54:28 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+ const futureData = data.list.filter((item) => {
+//     const itemTime = new Date(item.dt_txt);
+//     return itemTime > now; // keep only future entries
+//   });
+
+//   return futureData;
