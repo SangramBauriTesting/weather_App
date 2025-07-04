4f284419c99b1438cbedd4094a0e1bf3a8188e5e
SangramBauriTesting <sangramdeveloper1@gmail.com>
2025-07-04 16:40:42 +0530

    api call after click the get location btn

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { getCurrentWeatherUsingCoordinate } from "./api.js";
 import {CurrentWeatherUI , ForecastWeatherUI} from "./ui.js";
 
+
+//immediate excute when page load
 (async () => {
   const response = await getCurrentWeatherUsingCoordinate();
   console.log("Current weather data:", response.current);
@@ -10,3 +12,40 @@ import {CurrentWeatherUI , ForecastWeatherUI} from "./ui.js";
   CurrentWeatherUI(response.current);
   ForecastWeatherUI(response.forecast);
 })();
+
+
+//select search input 
+const userInput = document.getElementById("cityInput");
+
+userInput.addEventListener("keydown",(e)=>{
+   if (e.key === "Enter")
+    {
+        
+    }
+})
+
+
+//after click the location btn 
+
+const getLocationBtn = document.querySelector('.locationIcon');
+console.log(getLocationBtn);
+
+async function getWeatherDataAfterClickLoc() {
+  
+    const response = await getCurrentWeatherUsingCoordinate();
+    console.log("Current weather data:", response.current);
+    console.log("Forecast data:", response.forecast);
+    
+    CurrentWeatherUI(response.current);
+  ForecastWeatherUI(response.forecast);
+   
+ 
+}
+
+
+getLocationBtn.addEventListener('click',getWeatherDataAfterClickLoc);
+
+
+
+
+
